9d1637bc93d591ea273223fb00f293e37d960896
elsuizo <martin.noblia@openmailbox.org>
2017-04-18 00:14:42 -0300

    compile AVR C code and download in the Arduino board

---
+++ Avr32/Arduino_board/inc/main.h
@@ -0,0 +1,28 @@
+/* -------------------------------------------------------------------------
+@file main.h
+
+@date 11/25/16 19:58:27
+@author Martin Noblia
+@email martin.noblia@openmailbox.org
+
+@brief
+
+@detail
+
+Licence:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+---------------------------------------------------------------------------*/
+#include <avr/io.h>
+// #define F_CPU 8000000UL  #<{(| 8 Mhz |)}>#
+#include <util/delay.h>
+
